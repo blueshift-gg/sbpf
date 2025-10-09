3f652af945abbf0946040503af6e4de60b0657fe
bidhan-a <bidhan619@gmail.com>
2025-10-09 23:29:16 +0545

    change Register to struct

--- crates/common/src/instruction.rs
+++ crates/common/src/instruction.rs
@@ -2,29 +2,18 @@ use std::ops::Range;
 
 use crate::opcode::Opcode;
 
-#[derive(Debug, Clone, Copy, PartialEq)]
-#[repr(u8)]
-pub enum Register {
-    R0,
-    R1,
-    R2,
-    R3,
-    R4,
-    R5,
-    R6,
-    R7,
-    R8,
-    R9,
-    R10,
+#[derive(Debug, Clone)]
+pub struct Register {
+    pub n: u8,
 }
 
 impl Register {
     pub fn to_string(&self) -> String {
-        format!("r{}", *self as u8)
+        format!("r{}", self.n)
     }
 }
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone)]
 pub enum Number {
     Int(i64),
     Addr(i64),
